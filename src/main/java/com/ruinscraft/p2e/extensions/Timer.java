3a2f4334e47553ebe77e2718068a4811d33f1b6e
Dexton Anderson <contact@dextonanderson.com>
2018-02-23 11:31:17 -0600

    implement more of timer

--- src/main/java/com/ruinscraft/p2e/extensions/Timer.java
+++ src/main/java/com/ruinscraft/p2e/extensions/Timer.java
@@ -7,9 +7,11 @@ import com.intellectualcrafters.plot.commands.CommandCategory;
 import com.intellectualcrafters.plot.commands.RequiredType;
 import com.intellectualcrafters.plot.commands.SubCommand;
 import com.intellectualcrafters.plot.config.C;
+import com.intellectualcrafters.plot.object.Location;
 import com.intellectualcrafters.plot.object.Plot;
 import com.intellectualcrafters.plot.object.PlotPlayer;
 import com.intellectualcrafters.plot.util.MainUtil;
+import com.intellectualcrafters.plot.util.PlotGameMode;
 import com.plotsquared.general.commands.CommandDeclaration;
 import com.ruinscraft.p2e.P2Extension;
 
@@ -22,8 +24,28 @@ import com.ruinscraft.p2e.P2Extension;
 		requiredType = RequiredType.PLAYER)
 public class Timer extends SubCommand implements P2Extension {
 
-	private final Map<Plot, Long> plotTimers = new ConcurrentHashMap<>();
-
+	private final Map<Plot, Expire> plotTimers = new ConcurrentHashMap<>();
+	
+	private class Expire {
+		
+		private final long expireAt;
+		private final Runnable runOnExpire;
+		
+		public Expire(long expireAt, Runnable runOnExpire) {
+			this.expireAt = expireAt;
+			this.runOnExpire = runOnExpire;
+		}
+		
+		public long getExpireAt() {
+			return expireAt;
+		}
+		
+		public Runnable runOnExpire() {
+			return runOnExpire;
+		}
+		
+	}
+	
 	@Override
 	public void enable() {
 		
@@ -47,16 +69,62 @@ public class Timer extends SubCommand implements P2Extension {
 	}
 
 	@Override
+	// TODO: make it where you can set gamemode AND teleport players on expiry
 	public boolean onCommand(PlotPlayer plotPlayer, String[] args) {
 
-		Plot plot = plotPlayer.getCurrentPlot();
+		final Plot plot = plotPlayer.getCurrentPlot();
 
 		if (!plot.isOwner(plotPlayer.getUUID())) {
 			MainUtil.sendMessage(plotPlayer, C.NO_PLOT_PERMS);
 			return true;
 		}
+		
+		if (args.length > 0) {
+			
+			if (args[0].equalsIgnoreCase("cancel")) {
+				if (!plotTimers.containsKey(plot)) {
+					MainUtil.sendMessage(plotPlayer, "you dont have a plot timer running");
+					return false;
+				}
+				plotTimers.remove(plot);
+				MainUtil.sendMessage(plotPlayer, "Your plot timer has been canceled");
+			}
+			
+			// eg 3h1m, 30s, 1m20s
+			String timeString = args[0];
+			Runnable runWhenExpire = null;
+			
+			// check options
+			if (args.length > 1) {
+				
+				if (args[1].equalsIgnoreCase("teleport")) {
+					Location l = plotPlayer.getLocation();
+					runWhenExpire = () -> {
+						plot.getPlayersInPlot().forEach(p -> p.teleport(l));
+					};
+					MainUtil.sendMessage(plotPlayer, "All players in plot will teleport to the location you are currently standing at when the timer finishes");
+				}
+				
+				if (args[1].equalsIgnoreCase("gamemode")) {
+					if (args.length < 2) {
+						MainUtil.sendMessage(plotPlayer, "please specify a gamemode to set players to when the timer expires");
+						return false;
+					}
+					PlotGameMode gameMode = PlotGameMode.valueOf(args[2]);
+					runWhenExpire = () -> {
+						plot.getPlayersInPlot().forEach(p -> p.setGameMode(gameMode));
+					};
+					MainUtil.sendMessage(plotPlayer, "all players will be set to " + gameMode.getName() + " when the timer epires");
+				}
+				
+			}
+			
+		}
 
-
+		if (plotTimers.containsKey(plot)) {
+			MainUtil.sendMessage(plotPlayer, "You already have a plot timer running on this plot, /plot timer cancel");
+			return false;
+		}
 
 		return true;
 
